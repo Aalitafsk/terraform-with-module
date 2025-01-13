33bac8ee22c5bc7264274f75253ec3b7c7159b81
demo-user <mrsyedaltaf@gmail.com>
2025-01-13 09:13:51 +0530

    line 28 is modified

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,6 +1,9 @@
 variable "public-key-location" {}
 variable "ec2-type" {}
-variable "subnet" {}
+variable "subnet" {
+    description = "List of subnet IDs"
+    type        = list(string)
+}
 variable "abc-sg" {}
 variable "AZ" {}
 variable "env" {}
